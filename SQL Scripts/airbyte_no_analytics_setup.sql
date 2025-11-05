75063fbb09278e88425cd9b1f6b3c2f3efd54327
Erik Dietrich <erik@daedtech.com>
2025-11-05 12:06:48 -0500

    Tweaks

--- SQL Scripts/airbyte_no_analytics_setup.sql
+++ SQL Scripts/airbyte_no_analytics_setup.sql
@@ -1,6 +1,6 @@
+CREATE VIEW @client.airtable_ctas AS SELECT * FROM @client.@ctas_table;
 CREATE VIEW @client.airtable_keywords AS SELECT * FROM @client.@keywords_table;
 CREATE VIEW @client.airtable_link_placements AS SELECT * FROM @client.@link_placements_table;
-CREATE VIEW @client.airtable_ranking_results AS SELECT * FROM @client.@ranking_results_table;
 CREATE VIEW @client.airtable_seo_issues AS SELECT * FROM @client.@seo_issues_table;
 CREATE VIEW @client.airtable_url_history AS SELECT * FROM @client.@url_history_table;
 CREATE VIEW @client.airtable_urls AS SELECT * FROM @client.@urls_table;
